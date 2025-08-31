e85b520452d518ac23f2c4408d59eeb051fb7ce2
Your Name <you@example.com>
2025-08-31 08:58:06 +0300

    fixes

--- run.sh
+++ run.sh
@@ -33,10 +33,12 @@ PORT=8080
 USERNAME=32username
 PASSWORD=password
 
+echo ""
 echo -e "\033[90mip:\033[0m \033[32m$IP\033[0m"
 echo -e "\033[90mport:\033[0m \033[32m$PORT\033[0m"
 echo -e "\033[90musername:\033[0m \033[32m$USERNAME\033[0m"
 echo -e "\033[90mpassword:\033[0m \033[32m$PASSWORD\033[0m"
+echo ""
 
 echo "Запускаем SOCKS5 прокси"
 "$HOME/Library/Python/3.9/bin/pproxy" -l socks5://127.0.0.1:8080#$USERNAME:$PASSWORD -v
